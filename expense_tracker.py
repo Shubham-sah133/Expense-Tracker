55bcbfcff2bc468038e4986d558dc0c9869d21e4
shubham-sah133 <shubhmk17@gmail.com>
2026-08-01 23:43:38 +0530

    Add expense tracker

--- expense_tracker.py
+++ expense_tracker.py
@@ -21,44 +21,7 @@ def addExpense():
 
     print("✅ Expense added Successfully: ")
 
-
-def viewAllExpense():
-    for expense in expenses:
-        print(expense)
-
-def viewTotalSpending():
-    total = 0
-
-    for expense in expenses:
-        total += expense["amount"]
-
-    print("\n========================")
-    print("💰 Total Spending")
-    print("========================")
-    print(f"Total Expenses: ₹{total}")
-    print("========================")
-
-while True:
-    print("\n Welcome to Expense Tracker: 💸\n")
-    print("======= MENU =======")
-    print("1 Add Expense: ")
-    print("2 View All Expense: ")
-    print("3 View total spending: ")
-    print("4 Exit: \n")
-
-    choice = int(input("\nEnter your choice between 1 to 4: "))
-
-    match choice:
-        case 1:
-            addExpense()
-        case 2:
-            viewAllExpense()    
-        case 3:
-            viewTotalSpending()
-        case 4:
-            break
-        case _:
-            print("\n Invalid choice")   
+   
         
         
 
